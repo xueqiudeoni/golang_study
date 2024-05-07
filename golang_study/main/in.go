05f72ca8bf5d2739f4b6aa270212aba0637652b0
xueqiudeoni <swag@study>
2024-05-07 18:53:28 +0800

    Revert "workers"
    This reverts commit 28f9ae2a6d2b119800c672a606acf17ba3fba7aa.

--- golang_study/main/in.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-func main(){
-	//new分配内存，new函数的实参是一个类型而不是具体数值，其返回值是对应类型的指针，如*int
-	num:=new(int)
-	fmt.Printf("num:  type:%T,值:%v,add:%p,指针指向的值:%v",num,num,&num,*num)
-}
